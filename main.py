ed61c37321780f5404da6f9fe7fecdab46468db9
TheLegendOfLonk <danielzabielski123@gmail.com>
2021-04-14 22:43:34 +0200

    Changes

--- main.py
+++ main.py
@@ -95,6 +95,16 @@ def make_video(MAP, dic):
     #Get length of longest list
     moves = max(len(e) for e in dic.values())
     images = []
+    frame = MAP
+    #Make image of starting positions
+    for img, pos_list in dic.items():
+        img.move(*pos_list[0])
+        frame = frame + img
+    
+    for x in range(5):
+        images.append(deepcopy(frame))
+
+    #Make frames of every move
     for m in range(moves - 1):
         move_list = dict()
         for img, pos_list in dic.items():
@@ -103,15 +113,22 @@ def make_video(MAP, dic):
                 move_list[img] = temp_move
             except IndexError:
                 pass
+        logging.info(f'Visualizing move {m}')
         images.extend(make_move(move_list, MAP))
+        logging.info(f'Finished visualizing move {m}')
 
-    #Make video
-    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 25, (MAP.image.width/2, MAP.image.height))
+    #Creating videowriter
+    logging.info('Creating Videowriter')
+    out = cv2.VideoWriter('project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 25, (MAP.image.width, MAP.image.height))
+    logging.info('Created Videowriter')
  
+    #Writing video
+    logging.info('Writing video')
     for i in range(len(images)):
         x = images[i]
         x = x.convert()
         out.write(x.image)
+    logging.info('Finished writing video')
     out.release()
 
 def make_move(li, MAP):
@@ -164,7 +181,7 @@ def split(li):
 if __name__ == '__main__':
     #Logging setup
     FORMAT = '[%(levelname)s] - %(asctime)s: %(message)s'
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.INFO,
                         format=FORMAT,
                         filename='debug.log',
                         datefmt='%H:%M:%S')
@@ -178,14 +195,124 @@ if __name__ == '__main__':
     logging.info('Finished loading environment variables')
 
     #Load images
+    logging.info('Loading assets')
     MAP = Img('Assets\\Map.jpg')
     red = Img('Assets\\Red.png', 80, 80)
+    white = Img('Assets\\White.png', 80, 80)
+    logging.info('Finished loading assets')
     
+    destinations = {
+        1: (1373, 121),
+        2: (1983, 76),
+        3: (2375, 87),
+        4: (2611, 71),
+        5: (3568, 92),
+        6: (3863, 94),
+        7: (4166, 107),
+        8: (1254, 257),
+        9: (1492, 271),
+        10: (2183, 258),
+        11: (2362, 291),
+        12: (2511, 264),
+        13: (2784, 243),
+        14: (3063, 189),
+        15: (3353, 163),
+        16: (3630, 262),
+        17: (4147, 353),
+        18: (1115, 370),
+        19: (1337, 400),
+        20: (1628, 336),
+        21: (1913, 453),
+        22: (2372, 499),
+        23: (2600, 381),
+        24: (2952, 387),
+        25: (3104, 428),
+        26: (3337, 252),
+        27: (3378, 390),
+        28: (3484, 340),
+        29: (3872, 422),
+        30: (4245, 399),
+        31: (1206, 479),
+        32: (1526, 561),
+        33: (1783, 519),
+        34: (2201, 564),
+        35: (2447, 630),
+        36: (2546, 649),
+        37: (2706, 504),
+        38: (3031, 519),
+        39: (3179, 485),
+        40: (3433, 588),
+        41: (3544, 549),
+        42: (4156, 554),
+        43: (1055, 608),
+        44: (1390, 674),
+        45: (1614, 722),
+        46: (1822, 654),
+        47: (1990, 618),
+        48: (2252, 732),
+        49: (2628, 744),
+        50: (2783, 639),
+        51: (3107, 656),
+        52: (3275, 608),
+        53: (3463, 419),
+        54: (3589, 682),
+        55: (3878, 677),
+        56: (4260, 709),
+        57: (1158, 735),
+        58: (1481, 774),
+        59: (1548, 853),
+        60: (1671, 833),
+        61: (1885, 865),
+        62: (1998, 826),
+        63: (2276, 970),
+        64: (2445, 937),
+        65: (2620, 898),
+        66: (2727, 876),
+        67: (2909, 846),
+        68: (3139, 794),
+        69: (3349, 777),
+        70: (3609, 824),
+        71: (3838, 822),
+        72: (4080, 842),
+        73: (1157, 866),
+        74: (1272, 1003),
+        75: (1428, 958),
+        76: (1633, 948),
+        77: (1770, 1052),
+        78: (1931, 1032),
+        79: (2051, 1008),
+        80: (2327, 1064),
+        81: (2558, 1103),
+        82: (2642, 1042),
+        83: (2869, 1013),
+        84: (3019, 941),
+        85: (3151, 893),
+        86: (3382, 979),
+        87: (3624, 1036),
+        88: (3729, 1063),
+        89: (3819, 986),
+        90: (3990, 987),
+        91: (4225, 988),
+        92: (1069, 1111),
+        93: (1084, 1210),
+        94: (1290, 1167),
+        95: (1386, 1152),
+        96: (1870, 1224),
+        97: (1976, 1187),
+        98: (2115, 1141),
+        99: (2247, 1160),
+        100: (2478, 1228),
+
+
+    }
     moves = {
-        red: [(200, 500),(1100, 1800),(3000, 90),(200, 10)]
+        white: [(200, 500),(1100, 1800),(3000, 90),(200, 10)],
+        red: [(100, 100), (200, 200), (300, 300), (400, 400)]
     }
 
+    logging.info('Making video')
     make_video(MAP, moves)
+    logging.info('Finished making video')
 
     #clip = (VideoFileClip("project.avi"))
     #clip.write_gif("output.gif")
